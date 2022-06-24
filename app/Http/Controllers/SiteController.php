4a92d09d97e91fa724cc296206a739b79d468d22
acharyaarjun <legendarjun333@gmail.com>
2022-06-24 16:42:12 +0545

    Inital Setup

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function getHome(){
+        return view('site.home');
+    }
+}
---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My | Own | Website</title>
+    <link rel="stylesheet" href="{{ asset('site/bootstrap/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('site/fontawesome/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
+</head>
+<body>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro facere ipsa quam quod ducimus necessitatibus libero architecto debitis earum tempore non, accusamus vitae repellat? Officia molestiae enim quia sequi ullam.
+    </p>
+    <button class="btn btn-danger"><i class="fa-solid fa-house-user"></i></button>
+    
+    <script src="{{ asset('site/js/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('site/fontawesome/js/all.js') }}"></script>
+    <script src="{{ asset('site/js/script.js') }}"></script>
+</body>
+</html>
