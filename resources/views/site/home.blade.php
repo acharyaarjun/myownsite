4086b3ce137e3dad5c70d4f185310f01e289cdc4
acharyaarjun <legendarjun333@gmail.com>
2022-06-30 17:00:51 +0545

    Slider Section Completed Successfully

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -21,7 +21,7 @@
                         <i class="fas fa-phone"></i> 098-7654321
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-6 col-sm-hiden">
+                <div class="col-md-6 col-lg-6 col-hidden-sm">
                     <div class="header-icon">
                         <a href="#"><i class="fab fa-facebook"></i></a>
                         <a href="#"><i class="fab fa-twitter"></i></a>
@@ -73,6 +73,51 @@
       </div>
     </section>
     {{-- navbar section end here --}}
+
+    {{-- slider section starts here --}}
+    <section id="slider">
+      <div class="container-fluid">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="{{ asset('site/image/slider1.jpg') }}" class="d-block w-100" alt="Slider One">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="{{ asset('site/image/slider2.jpg') }}" class="d-block w-100" alt="Slider Two">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="{{ asset('site/image/slider3.jpg') }}" class="d-block w-100" alt="Slider Three">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </button>
+        </div>
+      </div>
+    </section>
+    {{-- slider section ends here --}}
 
     <script src="{{ asset('site/js/jquery.js') }}"></script>
     <script src="{{ asset('site/bootstrap/js/bootstrap.js') }}"></script>
