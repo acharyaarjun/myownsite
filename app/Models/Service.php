516feeca220783e5fbd75c21a1d3a055420d2280
Arjun Acharya <legendarjun333@gmail.com>
2022-08-05 16:59:07 +0545

    Database ma table banayeko

---
+++ app/Models/Service.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+}
+
+// modal chai singular rw aghillo letter capital huna paryo vaney table chai sabbai small letter rw plural huna paryo jastai ki modal = Service vayo vaney table chai services huna paryo
